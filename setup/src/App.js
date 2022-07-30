08715e5665c5020693ee5c2456347e46eb354f51
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-30 21:37:03 +0530

    prevState

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ErrorExample from './components/1-error-example';
 import UseStateBasics from './components/2-useState-basics';
 import UseStateArray from './components/3-useState-array';
+import UseStateCounter from './components/5-useState-counter';
 import data from './data';
 import List from './List';
 function App() {
@@ -9,7 +10,8 @@ function App() {
   <div className='container'>
     {/* <ErrorExample/> */}
     {/* <UseStateBasics/> */}
-    <UseStateArray/>
+    {/* <UseStateArray/> */}
+    <UseStateCounter/>
   </div>
   )
 }
