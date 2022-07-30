20b0ed4c8a55f72f359da77e0cc06bc9d3762975
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-30 19:29:16 +0530

    object useState

--- setup/src/components/4-useState-object.js
+++ setup/src/components/4-useState-object.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
+    const [person,setPerson] = useState({
+        name: 'peter',
+        age: 24,
+        message: 'random message',
+    })
+
+    const changeMessage = () => {
+
+        //use spread operartor always when using useState in an object 
+        //to obtain the values of the remaining parameters other than the parameter to be changed
+        
+        setPerson({...person,message:'hello world'})
+    }
+
   return (
-    <div></div>
+    <div>
+        <h3>{person.name}</h3>
+        <h3>{person.age}</h3>
+        <h3>{person.message}</h3>
+        <button className='btn' onClick={changeMessage}>Change Message</button>
+    </div>
   )
 }
 
