1cddb794da54d0623801a53480034a7932a57bbb
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-02 17:59:25 +0530

    ternary

--- setup/src/conditional-rendering/2-short-circuit.js
+++ setup/src/conditional-rendering/2-short-circuit.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 
 const ShortCircuit = () => {
     const[text,setText] = useState('')
-    const firstValue = text ||  'hello world'
-    const secondValue = text && 'hello world'
+    const[isError,setIsError] = useState(false)
+    // const firstValue = text ||  'hello world'
+    // const secondValue = text && 'hello world'
   return (
     <div>
         {/* <h1>{firstValue}</h1>
         <h1>Value:{secondValue}</h1> */}
         <h1>{text || 'john'}</h1>
-        {text && <h1>hello world</h1>}
+        <button onClick={() => setIsError(!error)}>Toggle error</button>
+        {isError && <h1>Error...</h1>}
+        {isError ? (<p>there is an error...</p>) : (<div><h2>No error</h2></div>)}
     </div>
   )
 }
